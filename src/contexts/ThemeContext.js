c0cca0fe28009ffb942c4cb9b3fc8a2f3dffbb8a
Cristian Chitiva <cychitivav@unal.edu.co>
2023-12-03 10:48:14 -0500

    Add theme change functionality to ThemeProvider

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,9 +1,33 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 export const ThemeContext = React.createContext();
 
 export function ThemeProvider({ children }) {
 	const [theme, setTheme] = useState("1");
+
+	useEffect(() => {
+		const elements = document.querySelectorAll("body, body *");
+
+		if (theme === "1") {
+			elements.forEach((element) => {
+				element.classList.remove("second-theme");
+				element.classList.remove("third-theme");
+				element.classList.add("first-theme");
+			});
+		} else if (theme === "2") {
+			elements.forEach((element) => {
+				element.classList.remove("first-theme");
+				element.classList.remove("third-theme");
+				element.classList.add("second-theme");
+			});
+		} else if (theme === "3") {
+			elements.forEach((element) => {
+				element.classList.remove("first-theme");
+				element.classList.remove("second-theme");
+				element.classList.add("third-theme");
+			});
+		}
+	}, [theme]);
 
 	return (
 		<ThemeContext.Provider value={{ theme, setTheme }}>
